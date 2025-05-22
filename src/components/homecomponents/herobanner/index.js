edec6ad1f77ebed1f34b0c1e0c4b90a2d243588c
krish vaghasiya <kvaghasiya705@gmail.com>
2025-05-22 11:14:21 +0530

    Feat Ui break issue 12

--- src/components/homecomponents/herobanner/index.js
+++ src/components/homecomponents/herobanner/index.js
@@ -115,6 +115,7 @@ export default function Herobanner() {
             <span>FOLLOWER</span>
             <h3>420K</h3>
           </div>
+          <div className={styles.show}></div>
           <div className={styles.coutingboxmain}>
             <span>Years Of Experience</span>
             <h3>10+</h3>
@@ -130,6 +131,7 @@ export default function Herobanner() {
               <span>Know More</span>
             </Link>
           </div>
+          <div className={styles.show}></div>
         </div>
       </div>
     </div>
